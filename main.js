70945ff69b9046484aeb70145c27204f94f6123f
Osamuyi <muyixone@yahoo.com>
2023-05-09 12:38:29 +0100

    Store the list items in local storage

--- main.js
+++ main.js
@@ -3,19 +3,31 @@ const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 let myLeads = [];
 
-saveButton.addEventListener('click', () => {
-  myLeads.push(inputEl.value);
-  renderItems();
-  inputEl.value = '';
-});
-
-function renderItems() {
-  let list = document.createElement('li');
+saveButton.addEventListener('click', function () {
+  //Check if input field is not empty
   if (!inputEl.value) {
     return;
   }
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  renderItems();
+
+  // Save data to local storage
+  // convert myLeads array to string as local storage can only store values as strings
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+});
+
+function renderItems() {
+  // Declare a variable to store the list items in
+  let listItem = '';
   for (let i = 0; i < myLeads.length; i++) {
-    list.innerHTML = `<a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>`;
+    listItem += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;
   }
-  ulEl.appendChild(list);
+  ulEl.innerHTML = listItem;
+}
+
+let localStorageFile = JSON.parse(localStorage.getItem('myLeads'));
+if (localStorageFile) {
+  myLeads = localStorageFile;
+  renderItems();
 }
