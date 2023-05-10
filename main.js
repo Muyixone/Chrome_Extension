d6ef6f0711c979a689468b67e92e49146bd771bc
Osamuyi <muyixone@yahoo.com>
2023-05-10 12:33:16 +0100

    Refactor the renderItems function to make it dynamic

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
-const saveButton = document.getElementById('input-btn');
+const saveInputButton = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const deleteItems = document.getElementById('del-btn');
 let myLeads = [];
+let localStorageFile = JSON.parse(localStorage.getItem('myLeads'));
 
-saveButton.addEventListener('click', function () {
+if (localStorageFile) {
+  myLeads = localStorageFile;
+  renderItems(myLeads);
+}
+
+saveInputButton.addEventListener('click', () => {
   //Check if input field is not empty
   if (!inputEl.value) {
     return;
   }
   myLeads.push(inputEl.value);
   inputEl.value = '';
-  renderItems();
+  renderItems(myLeads);
 
   // Save data to local storage
   // convert myLeads array to string as local storage can only store values as strings
   localStorage.setItem('myLeads', JSON.stringify(myLeads));
 });
 
-function renderItems() {
+function renderItems(leads) {
   // Declare a variable to store the list items in
   let listItem = '';
-  for (let i = 0; i < myLeads.length; i++) {
-    listItem += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;
+  for (let i = 0; i < leads.length; i++) {
+    listItem += `<li><a href="${leads[i]}" target="_blank">${leads[i]}</a></li>`;
   }
   ulEl.innerHTML = listItem;
 }
 
-let localStorageFile = JSON.parse(localStorage.getItem('myLeads'));
-if (localStorageFile) {
-  myLeads = localStorageFile;
-  renderItems();
-}
+deleteItems.addEventListener('click', () => {
+  localStorage.clear();
+  myLeads.length = 0;
+  ulEl.innerHTML = '';
+});
